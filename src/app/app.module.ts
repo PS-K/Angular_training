3beabf91996884ce524f995f8b363b22bf340a75
PSK <psk12370@gmail.com>
2020-07-31 11:43:08 +0530

    angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,27 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout' ;
-import 'hammerjs' ;
 import { MatListModule, MatList } from '@angular/material/list'
 import { MatGridListModule } from '@angular/material/grid-list'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
+import 'hammerjs' ;
+
 
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { from } from 'rxjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService }  from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService }  from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module'
+
 
 @NgModule({
   declarations: [
@@ -24,9 +31,13 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
